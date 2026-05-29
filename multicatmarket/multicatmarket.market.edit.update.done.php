291a6dc23c364742b38e0aeec02594674d06653b
sodium carbonate <webitproff@gmail.com>
2026-05-29 13:25:20 +0300

    Update multicatmarket.market.edit.update.done.php

--- multicatmarket/multicatmarket.market.edit.update.done.php
+++ multicatmarket/multicatmarket.market.edit.update.done.php
@@ -9,9 +9,9 @@ Hooks=market.edit.update.done
  * Multicat plugin for Market Module, CMF Cotonti Siena v.0.9.26, PHP v.8.4+, MySQL v.8.0
  * Filename: multicatmarket.market.edit.update.done.php
  * Purpose: Хук для market.edit.update.done, modules\market\inc\market.functions.php, str 796. Сохраняет категории (structure_id) после обновления страницы товара.
- * Date=2025-12-05
+ * Date=2026-05-29
  * @package multicat
- * @version 1.1.0
+ * @version 1.2.0
  * @author webitproff
  * @copyright Copyright (c) webitproff 2025 | https://github.com/webitproff
  * @license BSD
